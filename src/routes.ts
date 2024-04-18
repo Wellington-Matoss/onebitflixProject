5c0c88cbf147404c2dbb0623eff91afb62022e9d
Wellington Matoss <wellington.matos.neves@gmail.com>
2024-04-18 17:23:43 -0300

    streaming

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@
 import express from 'express'
 import { categoriesController } from './controllers/categoriesController'
 import { coursesController } from './controllers/courseController'
+import { episodesController } from './controllers/episodesController'
 
 const router = express.Router()
 
@@ -11,6 +12,9 @@ router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
 
 router.get('/courses/featured', coursesController.featured)
+router.get('/courses/newest', coursesController.newest)
+router.get('/courses/search', coursesController.search)
 router.get('/courses/:id', coursesController.show)
 
+router.get('/episodes/stream', episodesController.stream)
 export { router }
